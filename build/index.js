0fb25c1e59e9aeff52186cb7b2c19cbb27ac5546
=Yop789 <2000djpz@gmail.com>
2023-04-09 20:01:05 -0600

    eee

--- build/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var _app = _interopRequireDefault(require("./app"));
-require("./database.js");
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-_app["default"].set('port', process.env.PORT || 4400);
-_app["default"].listen(_app["default"].get('port'), function () {
-  console.log('server listen on port', 4400);
-});
