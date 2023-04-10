af0d4a9746e7ec659f26e22a490bc36c39bdec0b
=Yop789 <2000djpz@gmail.com>
2023-04-09 22:31:20 -0600

    ddsdvvfdv

--- src/database.js
+++ src/database.js
@@ -1,9 +1,8 @@
-import mongoose from "mongoose";
-
-mongoose.set('strictQuery', true);
-mongoose.connect(" mongodb+srv://2000djpz:lmXQUP3gN37hauJr@cluster0.npo2fgu.mongodb.net/test",{
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-.then(db => console.log('Db is connected'))
-.catch(error => console.log(error))
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://2000djpz:lmXQUP3gN37hauJr@cluster0.npo2fgu.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test");
+  // perform actions on the collection object
+  client.close();
+});
