34f52f1227071ceefe5993ab9edbc8a9224e1e2a
=Yop789 <2000djpz@gmail.com>
2023-04-09 21:52:11 -0600

    dcdcdcdcc

--- src/database.js
+++ src/database.js
@@ -1,13 +1,8 @@
 import mongoose from "mongoose";
 
 mongoose.set('strictQuery', true);
-mongoose.connect("mongodb+srv://2000djpz:lmXQUP3gN37hauJr@cluster0.npo2fgu.mongodb.net/?retryWrites=true&w=majority",{
-    useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverSelectionTimeoutMS: 10000, // aumenta el tiempo de espera de la selección del servidor
-  socketTimeoutMS: 45000, // aumenta el tiempo de espera del socket
-  keepAlive: true,
-  keepAliveInitialDelay: 300000
+mongoose.connect("mongodb+srv://2000djpz:lmXQUP3gN37hauJr@cluster0.npo2fgu.mongodb.net/test retryWrites=true&w=majority", {
+    useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1
 })
-.then(db => console.log('Db is connected'))
-.catch(error => console.log(error))
+    .then(db => console.log('Db is connected'))
+    .catch(error => console.log(error))
