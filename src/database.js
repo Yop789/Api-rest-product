c031cd8d400a31a1b9eadd57e83191415647314d
=Yop789 <2000djpz@gmail.com>
2023-04-10 01:01:51 -0600

    vffffvfvfv

--- src/database.js
+++ src/database.js
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
 
 mongoose.set('strictQuery', true);
-mongoose.connect("mmongodb+srv://2000djpz:lmXQUP3gN37hauJr@cluster0.npo2fgu.mongodb.net/?retryWrites=true&w=majority",{
+mongoose.connect("mmongodb+srv://2000djpz:lmXQUP3gN37hauJr@cluster0.npo2fgu.mongodb.net/test?retryWrites=true&w=majority",{
+  serverSelectionTimeoutMS: 5000
 })
 .then(db => console.log('Db is connected'))
 .catch(error => console.log(error))
