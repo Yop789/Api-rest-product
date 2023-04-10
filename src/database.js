e249c7e55ab6b7a5217f9375330f5627c7960dc7
=Yop789 <2000djpz@gmail.com>
2023-04-10 00:52:27 -0600

    jnjdnnhndjncjnvujfdja9jinfjhrn

--- src/database.js
+++ src/database.js
@@ -1,9 +1,7 @@
 import mongoose from "mongoose";
 
 mongoose.set('strictQuery', true);
-mongoose.connect("mmongodb+srv://2000djpz:<password>@cluster0.npo2fgu.mongodb.net/?retryWrites=true&w=majority",{
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+mongoose.connect("mmongodb+srv://2000djpz:lmXQUP3gN37hauJr@cluster0.npo2fgu.mongodb.net/?retryWrites=true&w=majority",{
 })
 .then(db => console.log('Db is connected'))
 .catch(error => console.log(error))
