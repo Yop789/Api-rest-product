731d4ef53b9343d3239e440bbce02ab25143e8dd
=Yop789 <2000djpz@gmail.com>
2023-04-09 22:12:11 -0600

    ,kmudnuuwejjdne

--- src/database.js
+++ src/database.js
@@ -1,8 +1,9 @@
-import {mongoose, ServerApiVersion} from "mongoose";
+import mongoose from "mongoose";
 
 mongoose.set('strictQuery', true);
-mongoose.connect("mongodb+srv://2000djpz:lmXQUP3gN37hauJr@cluster0.npo2fgu.mongodb.net/test retryWrites=true&w=majority", {
-    useNewUrlParser: true, useUnifiedTopology: true
+mongoose.connect("mongodb+srv://2000djpz:lmXQUP3gN37hauJr@cluster0.npo2fgu.mongodb.net/test retryWrites=true&w=majority",{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 })
-    .then(db => console.log('Db is connected'))
-    .catch(error => console.log(error))
+.then(db => console.log('Db is connected'))
+.catch(error => console.log(error))
