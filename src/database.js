efb660a5ba0e557ed774fc7db91e0d66018ec26d
=Yop789 <2000djpz@gmail.com>
2023-04-09 22:05:45 -0600

    ddddghhher

--- src/database.js
+++ src/database.js
@@ -2,7 +2,7 @@ import {mongoose, ServerApiVersion} from "mongoose";
 
 mongoose.set('strictQuery', true);
 mongoose.connect("mongodb+srv://2000djpz:lmXQUP3gN37hauJr@cluster0.npo2fgu.mongodb.net/test retryWrites=true&w=majority", {
-    useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1
+    useNewUrlParser: true, useUnifiedTopology: true
 })
     .then(db => console.log('Db is connected'))
     .catch(error => console.log(error))
