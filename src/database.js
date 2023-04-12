a209998cbbdfeb61c6112d56c72da7e517e288db
=Yop789 <2000djpz@gmail.com>
2023-04-12 13:42:17 -0600

    fjknk mfhnkvmnf

--- src/database.js
+++ src/database.js
@@ -1,8 +1,17 @@
 import mongoose from "mongoose";
 
 mongoose.set('strictQuery', true);
+mongoose.connect("mongodb://127.0.0.1:27017/BD_Dofest",{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(db => console.log('Db is connected'))
+.catch(error =>{
+  console.log('No se conecto a la local')
+  mongoose.set('strictQuery', true);
 mongoose.connect("mongodb+srv://2000djpz:lmXQUP3gN37hauJr@cluster0.npo2fgu.mongodb.net/?retryWrites=true&w=majority",{
   serverSelectionTimeoutMS: 5000
 })
 .then(db => console.log('Db is connected'))
 .catch(error => console.log(error))
+})
